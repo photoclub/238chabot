4be051bbb140c1fb2776ccf21a61931e7acba8e7
Charlotte Mae Efren <charlotte.efren@gmail.com>
2017-11-07 03:51:27 +0800

    test
    test

--- bot_tests.php
+++ bot_tests.php
@@ -2,6 +2,7 @@
 
 
 function getSlider($sender){
+/*
   $items = array();
 
   for($i=0;$i<5;$i++){
@@ -39,6 +40,29 @@ function getSlider($sender){
   $jsonData = '{"recipient":{"id":"'.$sender.'"},
         "message":'.$output.'
         }';
+*/
+
+  $answer = [
+  "attachment" => [
+  "type" => "template", 
+  "payload" => [
+  "template_type" => "generic", 
+  "elements" => [[
+  "title" => "Migrate your symfony application", 
+  "item_url" => "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/", 
+  "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg", 
+  "subtitle" => "Migrate your symfony application from Cpanel to Cloud.", 
+  "buttons" => [[
+  "type" => "web_url", 
+  "url" => "www.cloudways.com", 
+  "title" => "View Website"], 
+  ["type" => "postback", 
+  "title" => "Start Chatting", 
+  "payload" => "get started"]]
+  ]]
+  ]]];
+
+  $jsonData=formatText($sender,$answer);
 
   return $jsonData;
 }
