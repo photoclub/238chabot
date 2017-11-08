d73afa13eeb08ffa6f778bef0a34a9d5ed1a8cd2
pixelmai <pixelmai@users.noreply.github.com>
2017-11-08 18:25:13 +0800

    Reverting token to real values

--- bot_tokens.php
+++ bot_tokens.php
@@ -1,8 +1,8 @@
 <?php
 
 /* Required to setup bot to FB application messenger */
-  $access_token = ‘your access token’;
-  $verify_token = ‘your verify token’;
+  $access_token = 'EAABxniO6VYQBANVKSOirZB6pT7FHp2LGetRostBNQFZBn9yeY2ct2rrqtZAqqQcNPK0kJa014LmXyv7sP5Vk2K2jYs3Bn39fYwaEDp8hyIwxMwK2CVOQ6B2Qz37wFZAD1ctugY2NdZBkTSfA4roBToXVqZAN2KFWdxnLbttHcd1wZDZD';
+  $verify_token = 'mastermaimai';
 
   if(isset($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe') {
     $challenge = $_REQUEST['hub_challenge'];
