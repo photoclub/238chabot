4636aed291ed8f4bd2ab8e19993ff94430b98215
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-13 01:29:10 +0800

    Query to api and display menu

--- commands/recipe.php
+++ commands/recipe.php
@@ -1,5 +1,31 @@
 <?php
 
+$endpoint = 'http://www.recipepuppy.com/api/?';
+
 function getRecipe($viand) {
-    return $viand;
+    $resp = queryApi($viand);
+    $prompt = "Craving for {$viand}?\n";
+    foreach ($resp['results'] as $key => $value) {
+        $prompt .= "{$key} - ".$value['title']."\n";
+    }
+    $prompt .= 'Pick a number.';
+    $prompt .= hasMore($viand) ? "\nOr get 'More'": '';
+    return $prompt;
 }
+
+function queryApi($keyword) {
+    global $endpoint;
+    $payload = array(
+        'q' => $keyword,
+        'p' => 1
+    );
+    $url = $endpoint . http_build_query($payload);
+    $resp = json_decode(file_get_contents($url), true);
+    return $resp;
+}
+
+function hasMore($keyword) {
+    return count(queryApi($keyword)['results']) > 0;
+}
+
+getRecipe('adobo');
