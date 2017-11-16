fa844500095abf5be0231f333a90ed808ce7b1b9
Charlotte Mae Efren <charlotte.efren@gmail.com>
2017-11-16 13:52:16 +0800

    Back to Guzzle's bot version

--- bot.php
+++ bot.php
@@ -1,19 +1,13 @@
 <?php
+include 'guzzle/FbBot.php';
 
-/* Required to setup bot to FB application messenger */
-  $access_token = 'EAABxniO6VYQBANVKSOirZB6pT7FHp2LGetRostBNQFZBn9yeY2ct2rrqtZAqqQcNPK0kJa014LmXyv7sP5Vk2K2jYs3Bn39fYwaEDp8hyIwxMwK2CVOQ6B2Qz37wFZAD1ctugY2NdZBkTSfA4roBToXVqZAN2KFWdxnLbttHcd1wZDZD';
-  $verify_token = 'mastermaimai';
+$hubVerifyToken = 'mastermaimai';
+$accessToken = 'EAABxniO6VYQBANVKSOirZB6pT7FHp2LGetRostBNQFZBn9yeY2ct2rrqtZAqqQcNPK0kJa014LmXyv7sP5Vk2K2jYs3Bn39fYwaEDp8hyIwxMwK2CVOQ6B2Qz37wFZAD1ctugY2NdZBkTSfA4roBToXVqZAN2KFWdxnLbttHcd1wZDZD';
 
-  if(isset($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe') {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
+$bot = new FbBot();
+$bot->setHubVerifyToken($hubVerifyToken);
+$bot->setaccessToken($accessToken);
 
-    if ($hub_verify_token === $verify_token) {
-      header("HTTP/1.1 200 OK");
-      echo $challenge;
-      die;
-    }
-  }
-/* Required to setup bot to FB application messenger */
-
-?>
+$input = json_decode(file_get_contents('php://input'), true);
+$message = $bot->readMessage($input);
+$textmessage = $bot->sendMessage($message);
