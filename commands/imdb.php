a5970684a4f3ed4c5ddfbb98fea3ae66f31d45d7
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-17 11:31:17 +0800

    Format elements

--- commands/imdb.php
+++ commands/imdb.php
@@ -2,15 +2,24 @@
 
 $endpoint = "http://www.omdbapi.com/?apikey=86030ddd&";
 $defaultThumb = "https://cdn.theunlockr.com/wp-content/uploads/2012/04/IMDb.jpg";
+$sizePerRequest = 3;
 
 
 function getMovieRating($title, $extra_context=null) {
+    global $sizePerRequest;
     $page = 1;
+    $top = 0;
     $respSearch = querySearch($title, $page);
+
+    $elements = [];
     foreach ($respSearch["Search"] as $key => $value) {
-        $details = queryDetail($value["imdbID"]);
-        print_r($details);
+        $detail = queryDetail($value["imdbID"]);
+        $elements[] = formatElement($detail);
     }
+
+    $elements = array_slice($elements, $top, $sizePerRequest);
+    print_r($elements);
+    return $elements;
 }
 
 
@@ -18,7 +27,6 @@ function querySearch($title, $page) {
     global $endpoint;
     $payload = ["s" => $title, "page" => $page];
     $url = $endpoint . http_build_query($payload);
-    print_r($url);
     $resp = json_decode(file_get_contents($url), true);
     return $resp;
 }
@@ -30,6 +38,31 @@ function queryDetail($imdbID) {
     $url = $endpoint . http_build_query($payload);
     $resp = json_decode(file_get_contents($url), true);
     return $resp;
+}
+
+
+function formatElement($detail) {
+    global $defaultThumb;
+    $element = [
+        "title" => $detail["Title"],
+        "item_url" => "http://www.imdb.com/title/". $detail["imdbID"] ."/",
+        "image_url" => $detail["Poster"] ?: $defaultThumb,
+        "subtitle" => getRatings($detail),
+        "buttons" => [[
+            "type" => "web_url",
+            "url"  => "http://www.imdb.com/title/". $detail["imdbID"] ."/",
+            "title" => "Read reviews"
+        ]]
+    ];
+    return $element;
+}
+
+function getRatings($detail) {
+    $rating = "";
+    foreach ($detail["Ratings"] as $key => $value) {
+        $rating .= $value["Source"] ." ⭐ ". $value["Value"] ."\n";
+    }
+    return $rating;
 }
 
 
