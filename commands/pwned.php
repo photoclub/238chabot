356b8b8913d42a7287ec7b790d3bc9badbe11536
EC2 Default User <ec2-user@ip-172-31-58-190.ec2.internal>
2017-11-17 12:44:59 +0000

    to delete

--- commands/pwned.php
+++ commands/pwned.php
@@ -1,16 +1,12 @@
 <?php
 
-function getPwned($email) {
+function getSynonyms($word) {
   $command = " Please try again.\nPWNED <email address>";
   $email = trim(preg_replace('/\s\s+/', ' ', str_replace("\n", " ", $email)));
 
   if(!empty($email)){
-    if(checkValidity($email, 'email') == 1){
+    if(checkValidity($email, 'name') == 1){
       $output = '';
-      $url = 'https://haveibeenpwned.com/api/v2/breachedaccount/charlotte.efren@gmail.com';
-      $proc = json_decode(file_get_contents($url), true);
-
-      file_put_contents('test-pwned.txt', $answer);
 
 
 
