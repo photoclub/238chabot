a771aa3b3db444e961a4e66c4fcb7e278f59fbfc
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-19 22:06:51 +0800

    Add simple pagination for university

--- commands/university.php
+++ commands/university.php
@@ -1,17 +1,45 @@
 <?php
+include_once __DIR__ . '/../db_helper.php';
+include_once __DIR__  . '/helpers/helperFunctions.php';
 
-
-function getUniversity($uni) {
+function getUniversity($uni, $extra_context=null) {
   $command = " Please try again.\nUniversity <name>";
   $uni = trim(preg_replace('/\s\s+/', ' ', str_replace("\n", " ", $uni)));
+  $proc = array();
+  $originalProc = array();
+  $sizePerRequest = 3;
+  $top = 0;
 
-  if(!empty($uni)){
-    if(checkValidity($uni, 'name') == 1){
+  if (empty($uni)) {
+    $output = "Please input the name of the university.".$command;
+  } else {
+    $output = "University name is using invalid characters.".$command;
+    if (checkValidity($uni, 'name')) {
       $output = '';
       $url = 'http://universities.hipolabs.com/search?name='.urlencode($uni).'&country=philippines';
       $proc = json_decode(file_get_contents($url), true);
+      $originalProc = $proc;
 
-      if(!empty($proc)){
+      if (empty($proc)) {
+        $output = "University is not found on this list.".$command;
+      } else {
+        // check previous session
+        if ($extra_context) {
+            $log = getUserDataForCommand($extra_context["user_id"], "university");
+            if ($log && $log->recent_command == "university" && $log->message == $uni) {
+                $prevContext = (json_decode($log["context"], true));
+                $prevContext = $prevContext["context"];
+                $top = $prevContext["top"] + $prevContext["responseCount"];
+                $originalResponseCount = $prevContext["originalResponseCount"];
+                if ($prevContext["responseCount"] % $sizePerRequest != 0 ||
+                    $top >= $originalResponseCount) {
+                    // go to next page
+                    $top = 0;
+                }
+            }
+        }
+
+        $proc = array_slice($proc, $top, $sizePerRequest);
         $unicount=count($proc);
 
         $output = $output . "****************************\n";
@@ -22,29 +50,51 @@ function getUniversity($uni) {
           $output = $output . "Name: " . $proc[0]['name'] . 
           "\nWeb Page: ". $proc[0]['web_pages'][0] .
            ") \n\n";
-        }else{
+        } else {
           $output = $output . "Webpages:\n";
-          for($x = 0; $x < $unicount; $x++ ){
-            for($y = 0; $y < $unicount; $y++ ){
-              $output = $output . trim($proc[$x]['web_pages'][$y]) ." ";
-            }
+          foreach ($proc as $key => $value) {
+            $output .= $value['name'] .' '. join(' ', $value['web_pages']). "\n";
+          }
+          if ($top + $sizePerRequest >= count($originalProc) - 1) {
+            $output .= 'End of results.';
+          } else {
+            $output .= 'Type "university '. $uni .'" for next set.';
           }
         }
 
         if(strlen($output) > 640){
           $output = "Your search is to broad, please refine search.".$command;
         }
-
-      }else{
-        $output = "University is not found on this list.".$command;
       }
-    }else{
+    } else {
       $output = "University name is using invalid characters.".$command;
     }
-  }else{
-    $output = "Please input the name of the university.".$command;
+  }
+
+  // save session data
+  if ($extra_context) {
+      saveSessionData(
+          $extra_context["user_id"],
+          "university",
+          $uni,
+          [
+              "response" => $proc,
+              "context" => [
+                  "top" => $top,
+                  "responseCount" => count($proc),
+                  "originalResponseCount" => count($originalProc)
+              ]
+          ]
+      );
   }
 
   $answer = ['text' => $output];
+  print_r($answer);
   return $answer;
 }
+
+// getUniversity('University of Cebu', ['user_id' => '1234']);
+// getUniversity('University of the Philippines', ['user_id' => '1234']);
+// getUniversity('University of the Philippines', ['user_id' => '1234']);
+// getUniversity('University of Cebu', ['user_id' => '1234']);
+// getUniversity('University of the Philippines', ['user_id' => '1234']);
